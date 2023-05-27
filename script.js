4124de3973025cf394448547d5abb51df4e204cf
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-05-27 20:51:59 -0300

    arrumando os valores

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ formulario.addEventListener("submit", e => {
     e.preventDefault()
 
     const nomeCidade = e.target.nomeCidade.value
+    const estadoCidade = e.target.estadoCidade.value
     
-    axios.get(url)
-        .then(res => console.log(res))
+    axios.get(`${url}&city_name=${nomeCidade}, ${estadoCidade}`)
+        .then(res => console.log(res.data))
 })
