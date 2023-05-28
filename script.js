3f47825d0741573e332577478db82840c23e9ce6
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-05-27 21:05:35 -0300

    Verificação de clima, umidade e tempo

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ formulario.addEventListener("submit", e => {
     const estadoCidade = e.target.estadoCidade.value
     
     axios.get(`${url}&city_name=${nomeCidade}, ${estadoCidade}`)
-        .then(res => console.log(res.data))
+        .then(res => criarCard(res))
 })
+
+function criarCard(response) {
+    let h2 = document.createElement('h2')
+    let divData = document.createElement('div')
+    let temperatura = response.data.results.temp
+    let tempo = response.data.results.time
+    let humidity = response.data.results.humidity
+    let cidadeEstado = response.data.results.city
+
+    h2.classList.add('titulos')
+    divData.classList.add('infos')
+
+    h2.innerText = cidadeEstado
+    divData.appendChild(temperatura)
+    divData.appendChild(tempo)
+    divData.appendChild(humidity)
+
+    divResultado.appendChild(h2)
+    divResultado.appendChild(divData)
+}
